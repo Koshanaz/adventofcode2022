e90fd6e0e4cbbc363966dd99f646a4bc75215424
TrystanH <haus2811@saskpolytech.ca>
2022-12-01 17:15:57 -0600

    Started it up in Java here!

---
+++ src/advent/Advent1.java
@@ -0,0 +1,19 @@
+package advent;
+import java.io.FileReader;
+import java.io.FileNotFoundException; 
+import java.io.IOException;
+
+public class Advent1 {
+	
+	public int[] elfOutPut(FileReader fr, int len) throws FileNotFoundException, IOException {
+		result = int[];
+		
+		return result;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

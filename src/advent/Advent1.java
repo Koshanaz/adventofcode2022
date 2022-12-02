f313ea93e6d973ceb281c002b74543ed0ea01dd8
TrystanH <haus2811@saskpolytech.ca>
2022-12-01 18:17:46 -0600

    Added interesting output, and I believe it is ready to test with main file! (has passed all example tests)

--- src/advent/Advent1.java
+++ src/advent/Advent1.java
@@ -7,19 +7,36 @@ import java.util.ArrayList;
 
 
 public class Advent1 {
-	
 	// Making an Elf for Object related fun!
-	public class Elf {
+	// Really an int[2] would work haha
+	// Doesn't need to be public since everything's happening in this class,
+	// but who knows!
+	public static class Elf {
 		int elfCalories = 0;
 		int elfID;
+		public Elf() {
+			// Empty
+		}
 		public Elf(int elfID) {
+			// Initialize with ID
 			this.elfID = elfID;
+		}
+		public Elf(int elfID, int elfCalories) {
+			// Initialize with Both
+			this.elfID = elfID;
+			this.elfCalories = elfCalories;
 		}
 		public int Add(int elfAdd) {
 			return this.elfCalories += elfAdd;
 		}
+		public void SetCalories(int elfNew) {
+			this.elfCalories = elfNew;
+		}
 		public int GetCalories() {
 			return this.elfCalories;
+		}
+		public void SetID(int elfID) {
+			this.elfID = elfID;
 		}
 		public int GetID() {
 			return this.elfID;
@@ -28,38 +45,50 @@ public class Advent1 {
 	
 	// Gonna output an ArrayList of Integers for now
 	// Now it's an int array
-	public static int[] elfOutPut(String fileName) throws FileNotFoundException, IOException {
+	// Now it's just an int as it only asks for the total amount carried, but I will print his ID (he deserves it)
+	public static int elfOutPut(String fileName) throws FileNotFoundException, IOException {
 		// Declare the Stuff going in the BufferedReader!
 		FileReader elfFileReader = new FileReader(fileName);
 		BufferedReader elfBuffReader = new BufferedReader(elfFileReader);
 		// Read that stuff!
 		String elfLine = elfBuffReader.readLine();
 		// Some declarations
+		// Realized I only need one elf
 		ArrayList<Elf> elfResult = new ArrayList<Elf>();
-		int lineCount = 1;
-		// First is calorie record, second is ID
-		int elfRecord[] = {0,0};
+		int elfCount = 1;
+		// Replaced my int[] with an Elf for Holiday fun!
+		Elf elfOnShelf = new Elf();
+		Elf elfCurrent = new Elf(elfCount++);
 		// While there's still a line available, output it!
 		// After testing, we'll parse through for ints and new lines, adding them to an ArrayList!
 		while(elfLine != null) {
-			System.out.println("Line " + lineCount++ + ": " + elfLine);
+			// While instead of if block in case of multiple blanks in a row
+			while(elfLine.equals("")) {
+				// When a new Elf is about to be created, check the elves!
+				if(elfCurrent.GetCalories() > elfOnShelf.GetCalories()) {
+					// Announce the victor and replace the loser
+					System.out.println("Elf " + elfCurrent.GetID() + " replaces Elf " + elfOnShelf.GetID()
+					+ " with a difference of " + (elfCurrent.GetCalories() - elfOnShelf.GetCalories()));
+					elfOnShelf = elfCurrent;
+				}
+				// If it's an empty line, make a new elf
+				elfCurrent = new Elf(elfCount++);
+				// Go to next line
+				elfLine = elfBuffReader.readLine();
+			}
+			// Add value to currentElf
+			elfCurrent.Add(Integer.valueOf(elfLine));
+			System.out.println("Elf " + elfCurrent.elfID + " gained: " + Integer.valueOf(elfLine) + " (Total " + elfCurrent.GetCalories() + ")");
 			elfLine = elfBuffReader.readLine();
 		}
-		if(elfResult != null) {
-			for(Elf elfie : elfResult) {
-				// Check each Elf's calories, and if it beats record, record new record and ID
-				if(elfie.GetCalories() > elfRecord[0]) {
-					elfRecord[0] = elfie.GetCalories();
-					elfRecord[1] = elfie.GetID();
-				}
-			}
-		}
-		return elfRecord;
+		// Print the final victor
+		System.out.println("Elf " + elfOnShelf.GetID() + " wins with " + elfOnShelf.GetCalories() + " calories!");
+		return elfOnShelf.GetCalories();
 	}
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		// TODO Auto-generated method stub
-		int[] result = elfOutPut("./advent1input.txt");
+		System.out.print(elfOutPut("./advent1test.txt"));
 	}
 
 }

c877ee937dcd7338f374780903e6ad1d76aaeac7
TrystanH <haus2811@saskpolytech.ca>
2022-12-01 18:41:16 -0600

    Absolutely crushed part 2! Had one failure, then changed the conditions of my while block and made it compare in descending order if a record holder lost (makes sense in context of the code haha)

--- src/advent/Advent1.java
+++ src/advent/Advent1.java
@@ -53,14 +53,12 @@ public class Advent1 {
 		// Read that stuff!
 		String elfLine = elfBuffReader.readLine();
 		// Some declarations
-		// Realized I only need one elf
-		ArrayList<Elf> elfResult = new ArrayList<Elf>();
 		int elfCount = 1;
 		// Replaced my int[] with an Elf for Holiday fun!
 		Elf elfOnShelf = new Elf();
 		Elf elfCurrent = new Elf(elfCount++);
 		// While there's still a line available, output it!
-		// After testing, we'll parse through for ints and new lines, adding them to an ArrayList!
+		// After testing, we'll parse through for ints and new lines instead of just printing!
 		while(elfLine != null) {
 			// While instead of if block in case of multiple blanks in a row
 			while(elfLine.equals("")) {
@@ -85,10 +83,79 @@ public class Advent1 {
 		System.out.println("Elf " + elfOnShelf.GetID() + " wins with " + elfOnShelf.GetCalories() + " calories!");
 		return elfOnShelf.GetCalories();
 	}
+	
+	// Advent 1 part 2! Same as the first, save for the while block for checking so removed comments
+	public static int elfTop3OutPut(String fileName) throws FileNotFoundException, IOException {
+		FileReader elfFileReader = new FileReader(fileName);
+		BufferedReader elfBuffReader = new BufferedReader(elfFileReader);
+		String elfLine = elfBuffReader.readLine();
+		// Ended up needing the array back for part two let's goooo
+		Elf[] elfResult = {new Elf(), new Elf(), new Elf()};
+		int elfCount = 1;
+		Elf elfCurrent = new Elf(elfCount++);
+		// KeepGoing to make things easier
+		boolean keepGoing = true;
+		while(keepGoing) {
+			// While instead of if block in case of multiple blanks in a row
+			// Added null check first
+			while(elfLine == null || elfLine.equals("")) {
+				// When a new Elf is about to be created, check the elves!
+				// Check first elf
+				if(elfCurrent.GetCalories() > elfResult[0].GetCalories()) {
+					System.out.println("Elf " + elfCurrent.GetID() + " replaces the first Elf " + elfResult[0].GetID()
+					+ " with a difference of " + (elfCurrent.GetCalories() - elfResult[0].GetCalories()));
+					// Swap with temp so I can check the loser to the ones below it
+					// This caused my first failure
+					Elf temp = elfResult[0];
+					elfResult[0] = elfCurrent;
+					elfCurrent = temp;
+				// Check second
+				} if (elfCurrent.GetCalories() > elfResult[1].GetCalories()) {
+					System.out.println("Elf " + elfCurrent.GetID() + " replaces the first Elf " + elfResult[1].GetID()
+					+ " with a difference of " + (elfCurrent.GetCalories() - elfResult[1].GetCalories()));
+					Elf temp = elfResult[1];
+					elfResult[1] = elfCurrent;
+					elfCurrent = temp;
+				// Check third
+				} if (elfCurrent.GetCalories() > elfResult[2].GetCalories()) {
+					System.out.println("Elf " + elfCurrent.GetID() + " replaces the first Elf " + elfResult[2].GetID()
+					+ " with a difference of " + (elfCurrent.GetCalories() - elfResult[2].GetCalories()));
+					Elf temp = elfResult[2];
+					elfResult[2] = elfCurrent;
+					elfCurrent = temp;
+				}
+				if(elfLine == null) {
+					keepGoing = false;
+					break;
+				}
+				// If it's an empty line, make a new elf
+				elfCurrent = new Elf(elfCount++);
+				// Go to next line
+				elfLine = elfBuffReader.readLine();
+			}
+			if(!keepGoing) {
+				break;
+			}
+			// Add value to currentElf
+			elfCurrent.Add(Integer.valueOf(elfLine));
+			System.out.println("Elf " + elfCurrent.elfID + " gained: " + Integer.valueOf(elfLine) + " (Total " + elfCurrent.GetCalories() + ")");
+			elfLine = elfBuffReader.readLine();
+		}
+		int elfFinalTotal = 0;
+		for(Elf elfie : elfResult) {
+			elfFinalTotal += elfie.GetCalories();
+		}
+		// Print the final victors
+		System.out.println("First Elf " + elfResult[0].GetID() + " has " + elfResult[0].GetCalories() + " calories!");
+		System.out.println("Second Elf " + elfResult[1].GetID() + " has " + elfResult[1].GetCalories() + " calories!");
+		System.out.println("Third Elf " + elfResult[2].GetID() + " has " + elfResult[2].GetCalories() + " calories!");
+		System.out.println("The top 3 elves together carry " + elfFinalTotal  + " calories!");
+		return elfFinalTotal;
+	}
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		// TODO Auto-generated method stub
-		System.out.print(elfOutPut("./advent1input.txt"));
+		System.out.print(elfTop3OutPut("./advent1input.txt"));
 	}
 
 }
